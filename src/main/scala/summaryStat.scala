44761fddced536bf8e1f088143a2e6e7fa4c7c1a
niu86niu0120 <glniu@localhost.localdomain>
2015-03-24 16:15:47 +0800

    reduce the random generated data size

--- src/main/scala/summaryStat.scala
+++ src/main/scala/summaryStat.scala
@@ -102,7 +102,8 @@ object summaryStat {
     //TASK 5 - Random Data Generation
     // Generate a random double RDD that contains 1 million i.i.d. values drawn from the
     // standard normal distribution `N(0, 1)`, evenly distributed in 10 partitions.
-    val u = normalRDD(sc, 1000000L, 10)
+    //val u = normalRDD(sc, 1000000L, 10)
+    val u = normalRDD(sc, 100L, 3)
     // Apply a transform to get a random double RDD following `N(1, 4)`.
     val v = u.map(x => 1.0 + 2.0 * x)
     v.saveAsTextFile(args(4))
